ac9f0d6ce840ed6633039708893e41978d840884
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-09-30 18:38:27 +0200

    Exercise concluded.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,36 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
-// Import any additional modules you want to include below \/
+import "../styles/main.scss";
+import { data } from "./data.js";
 
+const sections = [
+  document.getElementById("cakes"),
+  document.getElementById("biscuits"),
+  document.getElementById("bread"),
+];
+const parseData = JSON.parse(data);
+const dataArr = Object.entries(parseData);
 
-// \/ All of your javascript should go here \/
+console.log(dataArr);
 
+dataArr.forEach((entryCards) => {
+  entryCards[1].map((card) => {
+    sections.forEach((section) => {
+      if (card.type === section.id) {
+        let cardDetails = `
+          <div class="card m-3" style="width: 18rem;">
+            <img src="${
+              card.image
+            }" class="card-img-top" alt="Baked Goods" height="200rem">
+            <div class="card-body">
+              <h4 class="card-title">${card.title}</h4>
+              <h6>${card.author}<h6>
+              <p class="border-top border-secondary pt-2">Ingredients: ${card.ingredients.join(
+                ", "
+              )}</p>
+            </div>
+          </div>`;
+
+        section.innerHTML += cardDetails;
+      }
+    });
+  });
+});
